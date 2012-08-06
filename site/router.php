c629063bf618b5c9bc23e0fd00b0a080c3311528
Dmitri Perunov <dmitri.perunov@gmail.com>
2012-08-06 16:00:33 +0700

    Update router to 2.5

--- site/router.php
+++ site/router.php
@@ -1,78 +1,51 @@
 <?php
-/**
- *
- *
- * @author		Ivinco LTD.
- * @package		Ivinco
- * @subpackage	SphinxSearch
- * @copyright	Copyright (C) 2011 Ivinco Ltd. All rights reserved.
- * @license     This file is part of the SphinxSearch component for Joomla!.
 
-   The SphinxSearch component for Joomla! is free software: you can redistribute it
-   and/or modify it under the terms of the GNU General Public License as
-   published by the Free Software Foundation, either version 3 of the License,
-   or (at your option) any later version.
-
-   The SphinxSearch component for Joomla! is distributed in the hope that it will be
-   useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with the SphinxSearch component for Joomla!.  If not, see
-   <http://www.gnu.org/licenses/>.
-
- * Contributors
- * Please feel free to add your name and email (optional) here if you have
- * contributed any source code changes.
- * Name							Email
- * Ivinco					<opensource@ivinco.com>
- *
- */
+defined('_JEXEC') or die;
 
 /**
- * @param	array
- * @return	array
+ * Function to parse route.
+ *
+ * @param   array   $query    URL query.
+ * @return  array
  */
 function SphinxSearchBuildRoute(&$query)
 {
     $segments = array();
-
-    // if no item id specified, try and get it.
-    if (!JArrayHelper::getValue($query, "Itemid")) {
-        $application = JFactory::getApplication("site");
-	$menus = $application->getMenu();
-	$items = $menus->getItems("link", "index.php?option=com_sphinxsearch");
-
-	if (count($items) > 0) {
-            $query["Itemid"] = $items[0]->id;
-	}
+    if(isset($query['view']))
+    {
+        //$segments[] = $query['view'];
+        unset($query['view']);
     }
-
-    if (isset($query['view'])) {
-        if ($query['view'] != "results" && $query['view'] != "basic") {
-            $segments[] = JArrayHelper::getValue($query, "view");
-	}
-
-	unset($query['view']);
-    }
+    if(isset($query['id']))
+    {
+        $segments[] = $query['id'];
+        unset($query['id']);
+    };
 
     return $segments;
 }
 
 /**
- * @param	array
- * @return	array
+ * Function to do actual routing.
+ *
+ * @param   array    $segments    Parts of URL query.
+ * @return  array
  */
 function SphinxSearchParseRoute($segments)
 {
     $vars = array();
+    $app  = JFactory::getApplication();
+    $menu = $app->getMenu();
+    $item = $menu->getActive();
 
-    $vars['option'] = 'com_sphinxsearch';
-
-    if (true == ($item = array_shift($segments))) {
-        $vars['view'] = $item;
+    $count = count($segments);
+    switch($item->query['view'])
+    {
+    case 'search':
+        $id = &$segments[$count-1];
+        $vars['id']   = $id;
+        $vars['view'] = 'search';
+        break;
     }
-
     return $vars;
 }
